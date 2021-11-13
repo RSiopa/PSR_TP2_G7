33b4ae0e893909cf966e6b6f46917339cf96502b
RSiopa <siopainacio98@ua.pt>
2021-11-13 16:36:39 +0000

    Initialization Done

--- ar_paint.py
+++ ar_paint.py
@@ -8,6 +8,10 @@
 # PSR, November 2021.
 # --------------------------------------------------
 import argparse
+import copy
+
+from numpy import zeros
+
 from color_segmenter import *
 
 parser = argparse.ArgumentParser(description='Definition of test mode')     # arguments
@@ -17,10 +21,36 @@ args = vars(parser.parse_args())
 
 def main():
 
+    # -----------------------------------------------------------
+    # Initialization
+    # -----------------------------------------------------------
+
+    # Read dictionary in Json file
     data = json.load(open(args['json']))
     dict_json = data['limits']
-    print(dict_json)
 
+    # Video capture setup
+    window_name = 'window_sketch'
+    cv2.namedWindow(window_name)
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FPS, 15)
+
+    # White image aka Sketch created
+    _, image = capture.read()
+    h = len(image)
+    w = len(image[0])
+    image_sketch = zeros([h, w, 3])
+
+    for y in range(h):
+        for x in range(w):
+            image_sketch[y, x] = [255, 255, 255]
+
+    # -----------------------------------------------------------
+    # Continuous Operation
+    # -----------------------------------------------------------
+
+    cv2.imshow(window_name, image_sketch)
+    cv2.waitKey()
 
 if __name__ == '__main__':
     main()
