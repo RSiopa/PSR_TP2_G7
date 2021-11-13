06bf2bfbe7603bfe4b25a841c5af4f1af6999c81
rodrigof1998 <rodrigo.ferreira@ua.pt>
2021-11-13 12:51:51 +0000

    Update color segmented file

---
+++ ar_paint.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.8
+
+from color_segmenter import *
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- color_segmenter.py
+++ color_segmenter.py
@@ -11,43 +11,119 @@ import json
 import cv2
 import numpy as np
 
+# ---------------------------------------------------
+# Global Variables
+# ---------------------------------------------------
+min_BH = 0
+max_BH = 255
+min_GS = 0
+max_GS = 255
+min_RV = 0
+max_RV = 255
 
-def nothing(x):
-    pass
+
+# Define functions of trackbars
+def minOnTrackbarBH(threshold):
+    """
+    Trackbar for the minimum threshold for the Blue (RGB) or Hue (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global min_BH
+    min_BH = threshold
+    print('Selected threshold ' + str(min_BH) + ' for limit min B/H')
+
+
+def maxOnTrackbarBH(threshold):
+    """
+    Trackbar for the maximum threshold for the Blue (RGB) or Hue (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global max_BH
+    max_BH = threshold
+    print('Selected threshold ' + str(max_BH) + ' for limit max B/H')
+
+
+def minOnTrackbarGS(threshold):
+    """
+    Trackbar for the minimum threshold for the Green (RGB) or Saturation (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global min_GS
+    min_GS = threshold
+    print('Selected threshold ' + str(min_GS) + ' for limit min G/S')
+
+
+def maxOnTrackbarGS(threshold):
+    """
+    Trackbar for the maximum threshold for the Green (RGB) or Saturation (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global max_GS
+    max_GS = threshold
+    print('Selected threshold ' + str(max_GS) + ' for limit max G/S')
+
+
+def minOnTrackbarRV(threshold):
+    """
+    Trackbar for the minimum threshold for the Red (RGB) or Value (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global min_RV
+    min_RV = threshold
+    print('Selected threshold ' + str(min_RV) + ' for limit min R/V')
+
+
+def maxOnTrackbarRV(threshold):
+    """
+    Trackbar for the maximum threshold for the Red (RGB) or Value (HSV) channel
+    :param threshold: the threshold for the channel in question. Datatype: int
+    """
+    global max_RV
+    max_RV = threshold
+    print('Selected threshold ' + str(max_RV) + ' for limit max R/V')
 
 
 def main():
     window_name = 'window - Ex3a'
     cv2.namedWindow(window_name)
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FPS, 15)
+    #capture.set(cv2.CAP_PROP_FPS, 15)
 
-    ranges = {'limits': {'B': {'max': 255, 'min': 0},
-                         'G': {'max': 255, 'min': 0},
-                         'R': {'max': 255, 'min': 0}}}
+    cv2.createTrackbar('min B', window_name, 0, 255, minOnTrackbarBH)
+    cv2.createTrackbar('max B', window_name, 255, 255, maxOnTrackbarBH)
+    cv2.createTrackbar('min G', window_name, 0, 255, minOnTrackbarGS)
+    cv2.createTrackbar('max G', window_name, 255, 255, maxOnTrackbarGS)
+    cv2.createTrackbar('min R', window_name, 0, 255, minOnTrackbarRV)
+    cv2.createTrackbar('max R', window_name, 255, 255, maxOnTrackbarRV)
 
-    cv2.createTrackbar('min B', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max B', window_name, 255, 255, nothing)
-    cv2.createTrackbar('min G', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max G', window_name, 255, 255, nothing)
-    cv2.createTrackbar('min R', window_name, 0, 255, nothing)
-    cv2.createTrackbar('max R', window_name, 255, 255, nothing)
+    # Set the trackbar position to 255 for maximum trackbars
+    cv2.setTrackbarPos('max B', window_name, 255)
+    cv2.setTrackbarPos('max G', window_name, 255)
+    cv2.setTrackbarPos('max R', window_name, 255)
+
+    # Initialize trackbars
+    minOnTrackbarBH(0)
+    maxOnTrackbarBH(255)
+    minOnTrackbarGS(0)
+    maxOnTrackbarGS(255)
+    minOnTrackbarRV(0)
+    maxOnTrackbarRV(255)
 
     while True:
         _, image = capture.read()
 
+        ranges = {'limits': {'B': {'min': min_BH, 'max': max_BH},
+                            'G': {'min': min_GS, 'max': max_GS},
+                            'R': {'min': min_RV, 'max': max_RV}}}
+
+        # Convert the dict structure created before to numpy arrays, because opencv uses it.
         mins = np.array([ranges['limits']['B']['min'], ranges['limits']['G']['min'], ranges['limits']['R']['min']])
         maxs = np.array([ranges['limits']['B']['max'], ranges['limits']['G']['max'], ranges['limits']['R']['max']])
 
-        ranges['limits']['B']['min'] = mins[0] = cv2.getTrackbarPos('min B', window_name)
-        ranges['limits']['G']['min'] = mins[1] = cv2.getTrackbarPos('min G', window_name)
-        ranges['limits']['R']['min'] = mins[2] = cv2.getTrackbarPos('min R', window_name)
-        ranges['limits']['B']['max'] = maxs[0] = cv2.getTrackbarPos('max B', window_name)
-        ranges['limits']['G']['max'] = maxs[1] = cv2.getTrackbarPos('max G', window_name)
-        ranges['limits']['R']['max'] = maxs[2] = cv2.getTrackbarPos('max R', window_name)
+        segmented = cv2.inRange(image, mins, maxs)
 
-        mask_black = cv2.inRange(image, mins, maxs)
-        cv2.imshow(window_name, mask_black)
+        cv2.imshow('Original', image)
+        cv2.imshow(window_name, segmented)
 
         if cv2.waitKey(1) == ord('w'):
             file_name = 'limits.json'
